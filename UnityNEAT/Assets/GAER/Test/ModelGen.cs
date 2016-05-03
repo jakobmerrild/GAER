6d224a8f33b5ffc15a0dfdb549fdd7c91386df91
Ivan Naumovski <inau@itu.dk>
2016-05-03 17:44:37 +0200

    added a physics tester, modelgen is incomplete

---
+++ UnityNEAT/Assets/GAER/Test/ModelGen.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MarchingCubesProject
+{
+    public class ModelGen
+    {
+
+        //not 100% random
+        public static float[,,] GenerateRandomModel(int dimSize)
+        {
+            float[,,] model = new float[dimSize, dimSize, dimSize];
+
+            System.Random gen = new System.Random();
+
+            int x, y, z, tmp;
+            tmp = 0;
+            for (x = 1; x < model.GetLength(0) - 1; x++)
+            {
+                for (y = 5; y < model.GetLength(1) - 1; y++)
+                {
+                    for (z = 1; z < model.GetLength(2) - 1; z++)
+                    {
+                        model[x, y, z] = gen.Next(2);
+                    }
+                }
+            }
+
+            return model;
+        }
+
+        //not 100% random
+        public static float[,,] GenerateSimpleChair(int dimSize)
+        {
+            float[,,] model = new float[dimSize, dimSize, dimSize];
+
+            System.Random gen = new System.Random();
+
+            int x, y, z, tmp;
+            tmp = 0;
+            for (x = 1; x < model.GetLength(0) - 1; x++)
+            {
+                int legSize = dimSize / 4;
+                for (y = 1; y < model.GetLength(1) - 1; y++)
+                {
+                    for (z = 1; z < model.GetLength(2) - 1; z++)
+                    {
+                        if (y > 5 && y < 7) model[x, y, z] = 1;
+                        if (x < legSize || x > (model.GetLength(0) - 1) - legSize) model[x, y, z] = 1;
+                    }
+                }
+            }
+
+            return model;
+        }
+    }
+
+    public class ModelRen
+    {
+        public struct comp
+        {
+            private int x;
+            private int y;
+            private GameObject component;
+
+            public comp(int x, int y, GameObject component)
+            {
+                this.x = x;
+                this.y = y;
+                this.component = component;
+            }
+        }
+
+        private readonly float[,,] model;
+
+        public ModelRen(float[,,] model)
+        {
+            this.model = model;
+        }
+
+        public GameObject Cubify()
+        {
+            var par = new GameObject("CubifiedModel");
+            return par;
+        }
+
+ /*       public List<comp> SliceModel(int layerIndex)
+        {
+            var components = new List<comp>();
+
+            for (int x = 0; x < model.GetLength(0); x++)
+            {
+                for (int z = 0; z < model.GetLength(2); z++)
+                {
+                    int startX, startZ, endX, endZ;
+                    if (model[x, layerIndex, z] == 1)
+                    {
+                        startX = x; endX = x;
+                        startZ = z; endZ = z;
+                        while (model[endX, layerIndex, endZ] == 1.0f) endX++;
+                        for (int dX = startX; dX <= endX; endZ++)
+                        {
+                            //check if adjacent row of same size exists
+                            while (model[dX, layerIndex, endZ] == 1.0f) dX++;
+                            if(dX < endX) break;
+                        }
+                        GameObject go = new GameObject();
+                        go.transform.localScale = new Vector3(endX-startX, 1, endZ-startZ);
+                        components.Add( new comp(startX, startZ, go) );
+                    }
+                }
+            }
+
+            return components;
+        }*/
+
+        public void PreProcessSlice(int y_index)
+        {
+            int[,] connected = new int[model.GetLength(0), model.GetLength(2)];
+
+            int cmp = 1;
+            for (int x = 0; x < connected.GetLength(0); x++)
+            {
+                for (int z = 0; z < connected.GetLength(1); z++)
+                {
+                    if (model[x, y_index, z] == 1)
+                    {
+                        connected[x, z] = cmp;
+
+                        //Adjacent right
+                        if (model[x + 1, y_index, z] == 1) connected[x + 1, z] = cmp;
+                        else cmp++;
+
+                        //Adjacent below
+                        if (model[x, y_index, z + 1] == 1) connected[x, z + 1] = cmp;
+                        else cmp++;
+                    }
+                }
+            }
+
+        }
+
+    }
+
+}
---
+++ UnityNEAT/Assets/GAER/Test/Tester.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Tester {
+
+    //margin between population members
+    readonly static int margin = 5;
+
+    public static IEnumerator Testpopulation(GameObject[] population, float width, float height, float length)
+    {
+        Vector3[] initPos = new Vector3[population.Length];
+
+        //used for placement of pop members
+        float mod = Mathf.Sqrt(population.Length);
+        Debug.Log("GridSize " + mod);
+
+        //Surface to place objects on
+        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        plane.transform.position = new Vector3(0, -(height / 2), 0);
+        plane.transform.localScale = new Vector3(width * mod, 1, height * mod);
+        plane.GetComponent<MeshCollider>().material = new PhysicMaterial();
+        
+        int iter = 0;
+        for (int i = 0; i < population.Length; i++)
+        {
+            if (i != 0 && i % mod == 0) iter++;
+
+            //Center mesh and 
+            population[i].transform.localPosition = new Vector3(    -width / 2 + ((width + margin) * (i % mod)),
+                                                                    -height / 2,
+                                                                    -length / 2 + ((length + margin) * iter)
+                                                       );
+
+            initPos[i] = population[i].transform.localEulerAngles;
+
+            createDropObject(population[i].transform.position, width, height, length);
+        }
+
+        yield return new WaitForSeconds(5);
+
+        float[] fit = new float[population.Length];
+        String formatted = "Results:\n";
+        for (int i = 0; i < population.Length; i++)
+        {
+            fit[i] = evaluate(initPos[i], population[i].transform.localEulerAngles);
+            formatted = String.Concat(formatted, fit[i]+"\n");
+        }
+        Debug.Log(formatted);
+
+    }
+
+    static GameObject createDropObject(Vector3 dropPoint, float width, float height, float length)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = new Vector3(5, 5, 5);
+        sphere.transform.localPosition = new Vector3(dropPoint.x + width / 2, dropPoint.y + height / 2 + 50, dropPoint.z + length / 2);
+        sphere.AddComponent<Rigidbody>();
+
+        return sphere;
+    }
+
+    static float evaluate(Vector3 before, Vector3 after)
+    {
+        return Mathf.Abs(
+                (before.x - after.x) +
+                (before.y - after.y) +
+                (before.z - after.z)
+            );
+    }
+}
