b835a63308faf946b789e3720b6287fedb52d438
Ivan Naumovski <inau@itu.dk>
2016-05-09 09:44:22 +0200

    Consolidated Ivan's work with master's (madafacker)

---
+++ UnityNEAT/Assets/GAER/Test/PhysicsTester.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PhysicsTester {
+
+    static GameObject createDropObject(Vector3 dropPoint, float width, float height, float length)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = new Vector3(5, 5, 5);
+        sphere.transform.localPosition = new Vector3(dropPoint.x + width / 2, dropPoint.y + height / 2 + 50, dropPoint.z + length / 2);
+        sphere.AddComponent<Rigidbody>();
+
+        return sphere;
+    }
+
+    static float evaluate(Vector3 before, Vector3 after)
+    {
+        return Mathf.Abs(
+                (before.x - after.x) +
+                (before.y - after.y) +
+                (before.z - after.z)
+            );
+    }
+}
